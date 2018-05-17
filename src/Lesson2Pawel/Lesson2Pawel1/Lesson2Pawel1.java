04b8217c6f94a7abcc3e408ca86c7e971d5939b0
kitainu <38722250+kitainu@users.noreply.github.com>
2018-05-17 09:55:24 +0200

    Update Lesson2Pawel1.java

--- src/Lesson2Pawel/Lesson2Pawel1/Lesson2Pawel1.java
+++ src/Lesson2Pawel/Lesson2Pawel1/Lesson2Pawel1.java
@@ -1,20 +1,19 @@
-package Lesson2Pawel.Lesson2Pawel1;
+package lesson2Pawel.lesson2Pawel1;
 
 public class Lesson2Pawel1 {
     public static void main(String[] args) {
-        int a = 7;
-        int v = 12;
-        double b = 4.3;
-        double q = 5.4;
-        double c = q  + b;
-        double d = a - v;
-        double z = a * a;
-        double l = q / b;
+        int firstNumb = 7;
+        int secondNumb = 12;
+        double thirdNumb = 4.3;
+        double fourthNumb = 5.4;
+        double adding = thirdNumb  + secondNumb;
+        double subtracion = fourthNumb - thirdNumb ;
+        double multiplication = firstNumb  * firstNumb ;
+        double division = thirdNumb / fourthNumb;
 
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(l);
-        System.out.println(z);
-
+        System.out.println(adding);
+        System.out.println(subtraction);
+        System.out.println(multiplication);
+        System.out.println(division);
     }
 }
