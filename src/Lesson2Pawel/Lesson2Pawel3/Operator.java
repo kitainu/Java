12410a9cfdb606a5f13d6cb787753a774afcab21
kitainu <38722250+kitainu@users.noreply.github.com>
2018-05-17 10:00:41 +0200

    Update Operator.java

--- src/Lesson2Pawel/Lesson2Pawel3/Operator.java
+++ src/Lesson2Pawel/Lesson2Pawel3/Operator.java
@@ -1,17 +1,13 @@
-package Lesson2Pawel.Lesson2Pawel3;
+package lesson2Pawel.lesson2Pawel3;
 
 public class Operator {
-
-
     public static void main(String[] args) {
         System.out.println(makes10(1, 9));
         System.out.println(makes10(9, 9));
         System.out.println(makes10(9, 10));
     }
-
         public static Boolean makes10 ( int a, int b){
             return ((a == 10) || (b == 10) || (a + b == 10));
-
         }
     }
 
