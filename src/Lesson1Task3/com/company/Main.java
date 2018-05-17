223bb15089f854c0a7705ae08a34a1d25141058d
kitainu <38722250+kitainu@users.noreply.github.com>
2018-05-17 09:33:33 +0200

    Update Main.java

--- src/Lesson1Task3/com/company/Main.java
+++ src/Lesson1Task3/com/company/Main.java
@@ -1,21 +1,15 @@
-package Lesson1Task3.com.company;
+package lesson1.task3.com.company;
 
-public class Main {
-
-
+public class main{
     public static void main(String[] args){
-        String Trademark = "Opel";
-        String Model = "Corsa";
-        int Year = 2010;
-        double Price =50.000;
-
+        String trademark = "Opel";
+        String model = "Corsa";
+        int year = 2010;
+        double price =50.000;
         System.out.println(Trademark );
         System.out.println(Model);
         System.out.println(Year);
         System.out.println(Price);
     }
-
-
-
-    }
+}
 
