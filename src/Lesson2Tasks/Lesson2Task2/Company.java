8e5460ec5fa95f4c8610627b786dcc2346e74a8c
kitainu <38722250+kitainu@users.noreply.github.com>
2018-05-17 10:04:28 +0200

    Update Company.java

--- src/Lesson2Tasks/Lesson2Task2/Company.java
+++ src/Lesson2Tasks/Lesson2Task2/Company.java
@@ -1,7 +1,6 @@
-package Lesson2Tasks.Lesson2Task2;
+package lesson2Tasks.lesson2Task2;
 
 public class Company {
-
     public static void main(String[] args) {
         Employee employee = new Employee("Stas", "Nowak", 4, 7);
 
@@ -10,8 +9,7 @@ public class Company {
         System.out.println("Nazwisko: " + employee.getLastName());
         System.out.println("wiek " + employee.getNumberOfWorkinYears());
         System.out.println("Lata pracy " + employee.getYearOfBirth());
-
-
+        
         Employee employee1 = new Employee("Jan", "Nowak", 2,15);
 
         employee1.pretendToWork();
@@ -22,12 +20,9 @@ public class Company {
 
         Employee employee2 = new Employee("Marta", "Klepacka",8,6);
 
-
         System.out.println("Imie: " + employee2.getFirstName());
         System.out.println("Nazwisko: " + employee2.getLastName());
         System.out.println("wiek " + employee2.getNumberOfWorkinYears());
         System.out.println("Lata pracy " + employee2.getYearOfBirth());
     }
-
-
 }
